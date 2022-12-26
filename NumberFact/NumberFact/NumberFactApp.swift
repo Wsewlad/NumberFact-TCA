b698cabb093a74af9c01422c22dd9be3ce6269e3
vfil <wladyslawfil@gmail.com>
2022-12-26 23:07:19 +0200

    Init project

---
+++ NumberFact/NumberFact/NumberFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberFactApp.swift
+//  NumberFact
+//
+//  Created by  Vladyslav Fil on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NumberFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
