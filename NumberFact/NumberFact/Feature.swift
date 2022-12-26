6321df181b198dece5673c2c4c38914ce39c1b90
vfil <wladyslawfil@gmail.com>
2022-12-27 00:00:23 +0200

    Add reduce methode

--- NumberFact/NumberFact/Feature.swift
+++ NumberFact/NumberFact/Feature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 struct Feature: ReducerProtocol {
     struct State: Equatable {
@@ -19,5 +20,41 @@ struct Feature: ReducerProtocol {
         case incrementButtonTapped
         case numberFactButtonTapped
         case numberFactResponse(TaskResult<String>)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .factAlertDismissed:
+            state.numberFactAlert = nil
+            return .none
+            
+        case .decrementButtonTapped:
+            state.count -= 1
+            return .none
+            
+        case .incrementButtonTapped:
+            state.count += 1
+            return .none
+            
+        case .numberFactButtonTapped:
+            return .task { [count = state.count] in
+                await .numberFactResponse(
+                    TaskResult {
+                        String(
+                            decoding: try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)/trivia")!).0,
+                            as: UTF8.self
+                        )
+                    }
+                )
+            }
+            
+        case let .numberFactResponse(.success(fact)):
+            state.numberFactAlert = fact
+            return .none
+            
+        case .numberFactResponse(.failure):
+            state.numberFactAlert = "Could not load a number fact :("
+            return .none
+        }
     }
 }
